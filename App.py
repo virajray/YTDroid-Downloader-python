1df269b7d05107aa8c25c3dd424744e9915ad630
Viraj Rathnayake <36956604+virajray@users.noreply.github.com>
2023-05-13 05:32:31 +0530

    Update App.py
    Basic downloader with added more features

--- App.py
+++ App.py
@@ -1,43 +1,80 @@
 import os
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
 from pytube import YouTube
+from tkinter import messagebox
+
+def CreateWidgets():
+
+    # Create a label for the title of the app.
+    head_label = Label(root, text="YouTube Playlist Downloader", font=("Inter bold", 36))
+    head_label.pack(pady=10)
 
 
-# Define a function to download the video
-def download_video():
-    # Get the URL of the video from the entry widget
-    video_url = url_entry.get()
+class YouTubeDownloader:
+    def __init__(self, master):
+        self.master = master
+        master.title("YouTube Downloader")
 
-    # Create a YouTube object
-    video = YouTube(video_url)
+        # Create a label and entry widget for the video URL
+        url_label = ttk.Label(master, text="Enter Video URL:")
+        url_label.pack()
+        self.url_entry = ttk.Entry(master, width=50)
+        self.url_entry.pack()
 
-    # Get the video stream
-    video_stream = video.streams.filter(only_audio=False).first()
+        # Create a label and combobox for the video quality
+        quality_label = ttk.Label(master, text="Select Video Quality:")
+        quality_label.pack()
+        self.quality_combobox = ttk.Combobox(master, values=["720p", "480p", "360p", "240p", "144p"])
+        self.quality_combobox.current(0)
+        self.quality_combobox.pack()
 
-    # Ask the user to select a directory to save the video in
-    save_dir = filedialog.askdirectory(title="Select Directory")
+        # Create a button to select the save directory
+        save_dir_button = ttk.Button(master, text="Select Save Directory", command=self.select_save_dir)
+        save_dir_button.pack()
 
-    # Download the video to the selected directory
-    video_stream.download(output_path=os.path.join(save_dir, video.title))
+        # Create a button to start the download
+        download_button = ttk.Button(master, text="Download", command=self.download_video)
+        download_button.pack()
 
-    # Show a message box when the video has been downloaded
-    tk.messagebox.showinfo(title="Download Complete", message="The video has been downloaded.")
+        # Create a progress bar to show the download progress
+        self.progress_bar = ttk.Progressbar(master, orient="horizontal", length=300, mode="determinate")
+        self.progress_bar.pack()
 
+        # Initialize the save directory to the current directory
+        self.save_dir = os.getcwd()
+
+    # Define a function to select the save directory
+    def select_save_dir(self):
+        self.save_dir = filedialog.askdirectory(title="Select Save Directory")
+
+    # Define a function to download the video
+    def download_video(self):
+        # Get the URL of the video from the entry widget
+        video_url = self.url_entry.get()
+
+        # Create a YouTube object
+        video = YouTube(video_url)
+
+        # Get the video stream with the selected quality
+        video_stream = video.streams.filter(res=self.quality_combobox.get(), only_audio=False).first()
+
+        # Download the video to the selected directory
+        video_stream.download(output_path=self.save_dir, filename_prefix="YouTube Downloader")
+
+        # Show a message box when the video has been downloaded
+        tk.messagebox.showinfo(title="Download Complete", message="The video has been downloaded.")
+
+    # Define a function to update the progress bar
+    def update_progress_bar(self, stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        progress = (bytes_downloaded / total_size) * 100
+        self.progress_bar["value"] = progress
 
 # Create the main window
 root = tk.Tk()
-root.title("YouTube Video Downloader")
-
-# Create a label and entry widget for the video URL
-url_label = tk.Label(root, text="Enter Video URL:")
-url_label.pack()
-url_entry = tk.Entry(root, width=50)
-url_entry.pack()
-
-# Create a button to start the download
-download_button = tk.Button(root, text="Download Video", command=download_video)
-download_button.pack()
-
-# Start the main event loop
+my_downloader = YouTubeDownloader(root)
 root.mainloop()
